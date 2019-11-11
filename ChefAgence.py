549bedcdcea6a41eb039ca1b6ad3e7e3b72e4121
Djikoloum <57571066+Djikoloum@users.noreply.github.com>
2019-11-11 07:04:39 +0100

    Create ChefAgence.py
    Djikoloum Jean Pierre Boudjidoum 18A898FS

---
+++ ChefAgence.py
@@ -0,0 +1,10 @@
+Class ChefAgence():
+   Soldde=int(input("solde"))
+   Prix=int(input("prix"))
+   if solde<=0;
+     Print("impossible")
+     else:
+      if solde<prix;
+        Print("impossible")
+        else:
+         Print("ok")
